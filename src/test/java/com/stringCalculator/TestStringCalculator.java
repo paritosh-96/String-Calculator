496ea41257edc2c9b040ca1d962726e76870c835
paritosh_18 <milendu.paritosh@customerxps.com>
2020-11-08 12:26:31 +0530

    2nd Iteration:
    Sum of any amount of numbers passed in a single string
    
    Signed-off-by: paritosh_18 <milendu.paritosh@customerxps.com>

--- src/test/java/com/stringCalculator/TestStringCalculator.java
+++ src/test/java/com/stringCalculator/TestStringCalculator.java
@@ -26,10 +26,14 @@ public class TestStringCalculator {
      */
     @Test
     public void testAdd() {
-        //positive test cases
+        //positive test cases with max two numbers
         assertEquals(0, calculator.add(""));
         assertEquals(1, calculator.add("1,"));
         assertEquals(3, calculator.add("1,2"));
         assertEquals(1, calculator.add(",1"));
+
+        //positive test cases with more than 2 numbers
+        assertEquals(36, calculator.add("1,2,3,4,5,6,7,8"));
+        assertEquals(160, calculator.add("10,20,30,40,50,10,"));
     }
 }
